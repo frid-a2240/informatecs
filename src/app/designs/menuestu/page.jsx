22e204ea6059b6c08f26f04458ac56a2f681b5a5
frida <frida22408@gmail.com>
2025-06-19 14:55:24 -0700

    se guardaron archivos

--- src/app/designs/menuestu/page.jsx
+++ src/app/designs/menuestu/page.jsx
@@ -7,7 +7,7 @@ export default function DashboardPage() {
 
   return (
     <div style={{ padding: '2rem' }}>
-      <h1 style={{ fontSize: '2rem' }}>¡Bien, {fullName}!</h1>
+      <h1 style={{ fontSize: '2rem' }}>¡Bienvenido, {fullName}!</h1>
       <p>Estás en la página principal del sistema.</p>
     </div>
   );
